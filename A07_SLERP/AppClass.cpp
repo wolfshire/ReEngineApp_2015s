05a9467fa51a944f9e501abbae0c76d341f22e54
unknown <jamie44344@gmail.com>
2016-03-28 18:42:57 -0400

    A06 done

--- A07_SLERP/AppClass.cpp
+++ A07_SLERP/AppClass.cpp
@@ -1,7 +1,7 @@
 #include "AppClass.h"
 void AppClass::InitWindow(String a_sWindowName)
 {
-	super::InitWindow("SLERP - YOUR USER NAME GOES HERE"); // Window Name
+	super::InitWindow("SLERP - Jamie Lamoureux"); // Window Name
 
 	//Setting the color to black
 	m_v4ClearColor = vector4(0.0f);
@@ -19,9 +19,10 @@ void AppClass::InitVariables(void)
 	m_pMeshMngr->LoadModel("Planets\\00_Sun.obj", "Sun");
 	m_pMeshMngr->LoadModel("Planets\\03_Earth.obj", "Earth");
 	m_pMeshMngr->LoadModel("Planets\\03A_Moon.obj", "Moon");
+	m_pMeshMngr->LoadModel("Planets\\02_Venus.obj", "Venus");
 
 	//Setting the days duration
-	m_fDay = 1.0f;
+	m_fDay = 1;
 }
 
 void AppClass::Update(void)
@@ -45,19 +46,87 @@ void AppClass::Update(void)
 	//Earth Orbit
 	double fEarthHalfOrbTime = 182.5f * m_fDay; //Earths orbit around the sun lasts 365 days / half the time for 2 stops
 	float fEarthHalfRevTime = 0.5f * m_fDay; // Move for Half a day
+	float fVenusHalfRevTime = 58.375 * m_fDay;
 	float fMoonHalfOrbTime = 14.0f * m_fDay; //Moon's orbit is 28 earth days, so half the time for half a route
+	float fVenusHalfOrbTime = 112.5f;
+
+	//Matrices
+	matrix4 m4Sun = IDENTITY_M4;
+	matrix4 m4Earth = m4Sun;
+	matrix4 m4Moon = m4Sun;
+	matrix4 m4Venus = m4Sun;
+	m4Sun *= glm::scale(vector3(5.936));
+
+	//Calculating percentages
+	float earthWhole;
+	float earthPercent = modff((float)fRunTime / fEarthHalfOrbTime, &earthWhole);
+	float spinWhole;
+	float spinPercent = modff((float)fRunTime / fEarthHalfRevTime, &spinWhole);
+	float moonWhole;
+	float moonPercent = modff((float)fRunTime / fMoonHalfOrbTime, &moonWhole);
+	float venusWhole;
+	float venusPercent = modff((float)fRunTime / fVenusHalfOrbTime, &venusWhole);
+	float venusRevWhole;
+	float venusRevPercent = modff((float)fRunTime / fVenusHalfRevTime, &venusRevWhole);
+
+	//Quaternions for rotation
+	quaternion rotStart = quaternion(vector3(0));
+	quaternion rotHalf = quaternion(vector3(0,PI,0));
+	quaternion rotWhole = quaternion(vector3(0, PI * 2, 0));
+
+	//Earth rotate/translate
+	if((int)earthWhole % 2 == 0)
+		m4Earth *= glm::mat4_cast(glm::mix(rotStart, rotHalf, earthPercent)) * glm::translate(11.0f, 0.0f, 0.0f);
+	else
+		m4Earth *= glm::mat4_cast(glm::mix(rotHalf, rotWhole, earthPercent)) * glm::translate(11.0f, 0.0f, 0.0f);	
+
+	//Moon rotate/translate
+	if((int)moonWhole %2 == 0)
+		m4Moon = m4Earth * glm::mat4_cast(glm::mix(rotStart, rotHalf, moonPercent)) * glm::translate(2.0f, 0.0f, 0.0f);
+	else
+		m4Moon = m4Earth * glm::mat4_cast(glm::mix(rotHalf, rotWhole, moonPercent)) * glm::translate(2.0f, 0.0f, 0.0f);
+
+	//Earth revolve
+	if ((int)spinWhole % 2 == 0)
+		m4Earth *= glm::mat4_cast(glm::mix(rotStart, rotHalf, spinPercent));
+	else
+		m4Earth *= glm::mat4_cast(glm::mix(rotHalf, rotWhole, spinPercent));
+
+	//Moon revolve
+	if((int)moonWhole % 2 == 0)
+		m4Moon *= glm::mat4_cast(glm::mix(rotWhole, rotHalf, moonPercent));
+	else
+		m4Moon *= glm::mat4_cast(glm::mix(rotHalf, rotStart, moonPercent));
+
+	//Venus rotate/translate
+	if ((int)earthWhole % 2 == 0)
+		m4Venus *= glm::mat4_cast(glm::mix(rotStart, rotHalf, venusPercent)) * glm::translate(7.95739644964f, 0.0f, 0.0f);
+	else
+		m4Venus *= glm::mat4_cast(glm::mix(rotHalf, rotWhole, venusPercent)) * glm::translate(7.95739644964f, 0.0f, 0.0f);
+
+	//Venus revolve
+	if ((int)venusRevWhole % 2 == 0)
+		m4Venus *= glm::mat4_cast(glm::mix(rotStart, rotHalf, venusRevPercent));
+	else
+		m4Venus *= glm::mat4_cast(glm::mix(rotHalf, rotWhole, venusRevPercent));
+
+	//Scaling the matrices
+	m4Earth *= glm::scale(vector3(0.524));
+	m4Moon *= glm::scale(vector3(0.27 * 0.524));
+	m4Venus *= glm::scale(vector3(0.524 / 1.05270828652));
 
 	//Setting the matrices
-	m_pMeshMngr->SetModelMatrix(IDENTITY_M4, "Sun");
-	m_pMeshMngr->SetModelMatrix(IDENTITY_M4, "Earth");
-	m_pMeshMngr->SetModelMatrix(IDENTITY_M4, "Moon");
+	m_pMeshMngr->SetModelMatrix(m4Sun, "Sun");
+	m_pMeshMngr->SetModelMatrix(m4Earth, "Earth");
+	m_pMeshMngr->SetModelMatrix(m4Venus, "Venus");
+	m_pMeshMngr->SetModelMatrix(m4Moon, "Moon");
 
 	//Adds all loaded instance to the render list
 	m_pMeshMngr->AddInstanceToRenderList("ALL");
-
-	static int nEarthOrbits = 0;
-	static int nEarthRevolutions = 0;
-	static int nMoonOrbits = 0;
+	
+	static int nEarthOrbits = earthWhole * 2;
+	static int nEarthRevolutions = spinWhole * 2;
+	static int nMoonOrbits = moonWhole * 2;
 
 	//Indicate the FPS
 	int nFPS = m_pSystem->GetFPS();
